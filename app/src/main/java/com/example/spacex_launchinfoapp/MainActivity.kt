d1d08330189c8f3742ad8fb11d5bd1f564434389
Lucy Holub <lusicomgolub@gmail.com>
2022-01-26 12:52:24 +0200

    #3: create main single activity.

--- app/src/main/java/com/example/spacex_launchinfoapp/MainActivity.kt
+++ app/src/main/java/com/example/spacex_launchinfoapp/MainActivity.kt
@@ -2,10 +2,68 @@ package com.example.spacex_launchinfoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.my_toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null) {
+            navigateToLaunchesScreen()
+            navigationView.setCheckedItem(R.id.nav_launches)
+        }
+    }
+
+    @Override
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_launches -> navigateToLaunchesScreen()
+            R.id.nav_ships -> navigateToShipsScreen()
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun navigateToShipsScreen() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, ShipsFragment()).commit()
+    }
+
+    private fun navigateToLaunchesScreen() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, LaunchesFragment()).commit()
     }
 }
---
+++ app/src/main/java/com/example/spacex_launchinfoapp/ShipsFragment.kt
@@ -0,0 +1,19 @@
+package com.example.spacex_launchinfoapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ShipsFragment : Fragment() {
+    @Override
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // todo fragment content will be implemented in scope of #5
+        return inflater.inflate(R.layout.fragment_ships, container,false)
+    }
+}
