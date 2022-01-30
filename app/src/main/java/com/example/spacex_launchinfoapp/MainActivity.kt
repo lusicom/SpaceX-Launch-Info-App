30a2a4fac83c3391156702bcad8424294920e7fc
Lucy Holub <lusicomgolub@gmail.com>
2022-01-30 11:01:19 +0200

    #3: create main single activity. (#7)
    * #3: create main single activity.
    
    * #3: create main single activity.
    made improvements according to PR review
    - added viewBinding
    - using NavigationComponent for Drawer
    
    * #3: create main single activity.
    - separated initialization of Navigation Drawer to the different fun

--- app/src/main/java/com/example/spacex_launchinfoapp/MainActivity.kt
+++ app/src/main/java/com/example/spacex_launchinfoapp/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.spacex_launchinfoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
+import com.example.spacex_launchinfoapp.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        initNavDrawer()
+    }
+
+    private fun initNavDrawer() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        drawerLayout = binding.drawerLayout
+        navigationView = binding.navView
+        navigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        findViewById<Toolbar>(R.id.my_toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
